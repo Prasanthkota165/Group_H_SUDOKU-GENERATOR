89cbf1046dcad6daa94848ee6816715485544f46
prasanthkota <prasanthkota@cmail.carleton.ca>
2020-05-18 14:48:39 -0400

    Sudoku Base

---
+++ sudoku_base.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void){
+int no_sudoku;
+printf("Input number of Sudoku patterns required: \n");  // Number of Sudoku Patterns will be one for the Release 1
+scanf("%d", &no_sudoku);
+return 0;
+}
+
