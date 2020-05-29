823dce6e402169758a189fdb09ff1a4b76c57f1c
gabriels2018 <gabrielseyoum@cmail.carleton.ca>
2020-05-29 14:20:38 -0400

    srcFiles

--- src/sudoku_base.c
+++ src/sudoku_base.c
@@ -12,25 +12,25 @@ int main(void){
     
   for(int i = 0; i < no_sudoku; i++){
 
-          = inputSudoku(&no_sudoku, &diff_level);
+          = inputSudoku(no_sudoku, diff_level);
 
-       generateSudokuTemplate(sudoku_template[ROWS][COLS]);
+       generateSudokuTemplate(sudoku_template);
 
-       rearrangeColsRows(sudoku_template[ROWS][COLS], rearranged_sudoku[ROWS][COLS]);
+       rearrangeColsRows(sudoku_template, rearranged_sudoku);
    
-       if (testSudokuRules(rearranged_sudoku[ROWS][COLS])){
-           applyMask(rearranged_sudoku[ROWS][COLS], diff_level, masked_sudoku[ROWS][COLS]);
+       if (testSudokuRules(rearranged_sudoku)){
+           applyMask(rearranged_sudoku, diff_level, masked_sudoku);
        }
        else{
-          generateSudokuTemplate(sudoku_template[ROWS][COLS]);
+          generateSudokuTemplate(sudoku_template);
        }
    
  
-       if (applySolver(masked_sudoku[ROWS][COLS])){
-           outputSudoku(masked_sudoku[ROWS][COLS], sudoku_solution [ROWS][COLS]);
+       if (applySolver(masked_sudoku)){
+           outputSudoku(masked_sudoku, sudoku_solution);
        }else
        {
-           applyMask(rearranged_sudoku[ROWS][COLS], diff_level, masked_sudoku[ROWS][COLS]);
+           applyMask(rearranged_sudoku, diff_level, masked_sudoku);
        }
 
    }
