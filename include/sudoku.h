30d360375895350114920036accdb58f7ae139ef
gabriels2018 <gabrielseyoum@cmail.carleton.ca>
2020-05-29 14:25:55 -0400

    headerFiles

--- include/sudoku.h
+++ include/sudoku.h
@@ -3,10 +3,10 @@
    #define COLS 9
 #endif  
 
-int [] inputSudoku(int no_sudoku, int diff_level);
-void generateSudokuTemplate(sudoku_temp[ROWS][COLS]);
-void rearrangeColsRows(int sudoku_temp[ROWS][COLS], rearranged_sudoku_temp[ROWS][COLS]);
+int [] inputSudoku();
+void generateSudokuTemplate(int sudoku_temp[ROWS][COLS]);
+void rearrangeColsRows(int sudoku_temp[ROWS][COLS], int rearranged_sudoku_temp[ROWS][COLS]);
 bool testSudokuRules(int test_sudoku[ROWS][COLS]);
-void applyMask(int test_sudoku[ROWS][COLS], int diff_level, masked_sudoku[ROWS][COLS]);
+void applyMask(int test_sudoku[ROWS][COLS], int diff_level, int masked_sudoku[ROWS][COLS]);
 bool applySolver(int sudoku_puzzle [ROWS][COLS]);
 void outputSudoku(int sudoku_puzzle [ROWS][COLS], int sudoku_solution [ROWS][COLS]);
