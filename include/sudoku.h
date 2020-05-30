a126bd1cb9d288098a74f5b98c09ac361b2661b5
Mathieufalardeau3 <65470001+Mathieufalardeau3@users.noreply.github.com>
2020-05-30 00:18:08 -0400

    Update sudoku.h 2
    Made sure the sudoku.h and .c are same in the output.

--- include/sudoku.h
+++ include/sudoku.h
@@ -10,4 +10,5 @@ bool testSudokuRules(int test_sudoku[ROWS][COLS]);
 void applyMask(int test_sudoku[ROWS][COLS], int diff_level, int masked_sudoku[ROWS][COLS]);
 bool applySolver(int sudoku_puzzle [ROWS][COLS]);
 void output_sudoku(int sudoku_solution [ROWS][COLS], int sudoku_puzzle [ROWS][COLS], int diff_lvl);
+void create_sudoku_file(int sudoku[ROWS][COLS], char* sudoku_type, int diff_lvl, int output_count);
 
