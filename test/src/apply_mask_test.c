a914b599695e6af67070c2b91039a567801cbfd4
gabriels2018 <gabrielseyoum@cmail.carleton.ca>
2020-05-31 22:53:26 -0400

    apply_mask_test

--- test/src/apply_mask_test.c
+++ test/src/apply_mask_test.c
@@ -7,7 +7,7 @@
 /**
 * apply_mask_test 
 * --------------- 
-* Tests apply_mask function
+* It tests apply_mask function
 * Language C
 * Author Gabriel Seyoum, Group-H member, student # 100333091
 * Date May 31, 2020
