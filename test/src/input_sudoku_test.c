c3dcebdbd6c35e77dafe66f08bc8fef8786ddffc
Rishith Rao <rishithrao@cmail.carleton.ca>
2020-06-07 14:18:29 -0400

    Minor changes for comments

--- test/src/input_sudoku_test.c
+++ test/src/input_sudoku_test.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "../../src/input_sudoku.c"
 
+/*
+* TITLE      : input_sudoku_test.c 
+*
+* Purpose   : Generates a pre-defined sudoku templete
+*
+* Language  : C
+*
+* Author    : Rishith Rao, 
+* GitHub ID : rishithrao57 
+* Date      : 7 June 2020
+*
+**/
+
 int main(){
 int diff_level,no_sudoku;
 
