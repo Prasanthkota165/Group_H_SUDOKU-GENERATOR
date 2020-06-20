220d1f83152090f34ed9c97ba45370d770dd133a
gabriels2018 <gabrielseyoum@cmail.carleton.ca>
2020-06-20 11:04:54 -0400

    comments

--- test/src/apply_mask_test.c
+++ test/src/apply_mask_test.c
@@ -5,7 +5,7 @@
 
 #ifndef __MY_HEADER__
 #define __MY_HEADER__
-     #include "../../include/sudoku.h"
+    #include "../../include/sudoku.h"
 #endif
 
 #include "../../src/apply_mask.c"
@@ -25,29 +25,28 @@
 */
 
 
-int main()
-{
+int main(){
    FILE *fp;
    int masked_sudoku [9][9], diff_level [4] = {0, 1, 2, 3};
    int cells_tobe_masked, numbers_tobe_shown;
    int array_size = sizeof diff_level / sizeof *diff_level;
    srand(time(0));
    int rearranged_sudoku1 [9][9] = {{1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                   {4, 5, 6, 7, 8, 9, 1, 2, 3},
-                                   {7, 8, 9, 1, 2, 3, 4, 5, 6},
-                                   {2, 3, 1, 5, 6, 4, 8, 9, 7},
-                                   {5, 6, 4, 8, 9, 7, 2, 3, 1},
-                                   {8, 9, 7, 2, 3, 1, 5, 6, 4},
-                                   {3, 1, 2, 6, 4, 5, 9, 7, 8},
-                                   {6, 4, 5, 9, 7, 8, 3, 1, 2},
-                                   {9, 7, 8, 3, 1, 2, 6, 4, 5}};
+                                    {4, 5, 6, 7, 8, 9, 1, 2, 3},
+                                    {7, 8, 9, 1, 2, 3, 4, 5, 6},
+                                    {2, 3, 1, 5, 6, 4, 8, 9, 7},
+                                    {5, 6, 4, 8, 9, 7, 2, 3, 1},
+                                    {8, 9, 7, 2, 3, 1, 5, 6, 4},
+                                    {3, 1, 2, 6, 4, 5, 9, 7, 8},
+                                    {6, 4, 5, 9, 7, 8, 3, 1, 2},
+                                    {9, 7, 8, 3, 1, 2, 6, 4, 5}};
 
     printf("\n*****apply_mask function test: test results are written to a log file (test/log/apply_mask_test_log.txt)*****\n");
     printf("\n");
 
     fp = fopen ("../log/apply_mask_test_log.txt", "w+");
-    if (fp == NULL)
-    {
+    
+    if (fp == NULL){
         printf("Could not open the file");
         _Exit(EXIT_FAILURE);
     }
@@ -85,38 +84,52 @@ int main()
 
     fprintf(fp, "%s", "\nPRECONDITION: negative test cases and the expected outputs\n");
     fprintf(fp, "%s", "----------------------------------------------------------\n");
-    fprintf(fp, "%s", "PRECONDITIONs are asserted at the function level, therefore only error messages will display here \n");
+    fprintf(fp, "%s", "The PRECONDITIONs were asserted at the function level and apply_mask function is designed to cover all cases. \n");
+    fprintf(fp, "%s", "When an incorrect input values are passed the function, the function displays an Error Message to the screen and terminate.\n");
+
     fprintf(fp, "%s", "\n");
     fprintf(fp, "%s", "*****************The outcome of the test*****************\n");
+
     fprintf(fp, "%s", "The rearranged_sudoku array size not being 9x9,  testing with 4x9 array\n");
     /* The case where the rearranged_sudoku array size not being 9x9, in this case being 4x9 */
     int rearranged_sudoku2 [4][9] = {{1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                    {4, 5, 6, 7, 8, 9, 1, 2, 3},
-                                    {7, 8, 9, 1, 2, 3, 4, 5, 6},
-                                    {2, 3, 1, 5, 6, 4, 8, 9, 7}};
+                                     {4, 5, 6, 7, 8, 9, 1, 2, 3},
+                                     {7, 8, 9, 1, 2, 3, 4, 5, 6},
+                                     {2, 3, 1, 5, 6, 4, 8, 9, 7}};    /* apply_mask function being called with rearranged_sudoku array size 4x9 instead of 9x9 */
 
-    /* apply_mask function being called with rearranged_sudoku array size 4x9 instead of 9x9 */
     apply_mask(rearranged_sudoku2, 1 , masked_sudoku);
-
     fprintf(fp, "%s", "The rearranged_sudoku array value(s) not being from 1 to 9\n");
-    /* The case where the rearranged_sudoku array value(s) not being from 1 to 9*/
-    int rearranged_sudoku3 [9][9] = {{1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                    {4, 5, 6, 7, 8, 9, 1, 2, 3},
-                                    {7, 8, 9, 1, 2, 3, 4, 5, 6},
-                                    {2, 3, 1, 5, 6, 4, 8, 9, 7},
-                                    {5, 6, 4, 8, 9, 7, 2, 3, 1},
-                                    {8, 9, 7, 2, 3, 1, 5, 6, 4},
-                                    {3, 1, 2, 6, 4, 5, 9, 7, 8},
-                                    {6, 4, 5, 9, 7, 8, 3, 1, 2},
-                                    {9, 77, 8, 3, 1, 2, 6, 4, 5}};
-
     /* apply_mask function being called with rearranged_sudoku value not being 9x9, in this case row 9 and column 2 being 77 */
+    int rearranged_sudoku3 [9][9] = {{1, 2, 3, 4, 5, 6, 7, 8, 9},
+                                     {4, 5, 6, 7, 8, 9, 1, 2, 3},
+                                     {7, 8, 9, 1, 2, 3, 4, 5, 6},
+                                     {2, 3, 1, 5, 6, 4, 8, 9, 7},
+                                     {5, 6, 4, 8, 9, 7, 2, 3, 1},
+                                     {8, 9, 7, 2, 3, 1, 5, 6, 4},
+                                     {3, 1, 2, 6, 4, 5, 9, 7, 8},
+                                     {6, 4, 5, 9, 7, 8, 3, 1, 2},
+                                     {9, 77, 8, 3, 1, 2, 6, 4, 5}};    /* The case where the rearranged_sudoku array value(s) not being from 1 to 9 */
+
+    fprintf(fp, "%s", "\n");
+    fprintf(fp, "%s", "Error Message: The rearranged_sudoku array value(s), data type, or array size is incorrect.\n");
     apply_mask(rearranged_sudoku3, 2, masked_sudoku);
 
     fprintf(fp, "%s", "The diff_level value not being 0, 1, 2, or 3\n");
-    /* The diff_level value not being 0, 1, 2, or 3 */
-    apply_mask(rearranged_sudoku1, 7 , masked_sudoku);
+    apply_mask(rearranged_sudoku1, 7 , masked_sudoku);    /* The diff_level value not being 0, 1, 2, or 3 */
+    apply_mask(rearranged_sudoku1, 'A' , masked_sudoku);  /* Incorrect diff_level data type */
 
+    /* The case where the rearranged_sudoku array data type is not integer */
+    float rearranged_sudoku4 [9][9] = {{1, 2, 3, 4, 5, 6, 7, 8, 9},
+                                       {4, 5, 6, 7, 8, 9, 1, 2, 3},
+                                       {7, 8, 9, 1, 2, 3, 4, 5, 6},
+                                       {2, 3, 1, 5, 6, 4, 8, 9, 7},
+                                       {5, 6, 4, 8, 9, 7, 2, 3, 1},
+                                       {8, 9, 7, 2, 3, 1, 5, 6, 4},
+                                       {3, 1, 2, 6, 4, 5, 9, 7, 8},
+                                       {6, 4, 5, 9, 7, 8, 3, 1, 2},
+                                       {9, 7, 8, 3, 1, 2, 6, 4, 5}};
+
+    apply_mask(rearranged_sudoku4, 0 , masked_sudoku);
 
     fclose(fp);
     return 0;
