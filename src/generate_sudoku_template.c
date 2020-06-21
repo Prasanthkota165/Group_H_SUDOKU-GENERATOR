c327ce48d653b11d593f8509081ee6301e25cfd7
Rishith Rao <rishithrao@cmail.carleton.ca>
2020-06-21 05:03:00 -0400

    comments

--- src/generate_sudoku_template.c
+++ src/generate_sudoku_template.c
@@ -18,9 +18,8 @@
  * This fnction generates the sudoku templete of size 9X9.      
  */
 void generate_sudoku_template(int sudoku_temp [9][9]){
- 
-	int i,j;
-	int a[9][9]= {{1,2,3,4,5,6,7,8,9}, 
+    int i,j;
+    int a[9][9]= {{1,2,3,4,5,6,7,8,9}, 
 	   		{4,5,6,7,8,9,1,2,3},
 	   		{7,8,9,1,2,3,4,5,6},
 	   		{2,3,1,5,6,4,8,9,7},
@@ -29,11 +28,10 @@ void generate_sudoku_template(int sudoku_temp [9][9]){
 	   		{3,1,2,6,4,5,9,7,8},
     		        {6,4,5,9,7,8,3,1,2},
 	   		{9,7,8,3,1,2,6,4,5}};
-	
-	for(i =0;i<9;i++){
-		for(j =0;j<9;j++){
-			sudoku_temp[i][j]=a[i][j];
-		}
+    for(i =0;i<9;i++){
+        for(j =0;j<9;j++){
+            sudoku_temp[i][j]=a[i][j];
 	}
+    }
  
 }
