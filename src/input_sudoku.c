9c1cd9b753444f097e15a3e9f12ec3738c24066a
Rishith Rao <rishithrao@cmail.carleton.ca>
2020-06-21 10:51:22 -0400

    Minor changes

--- src/input_sudoku.c
+++ src/input_sudoku.c
@@ -29,12 +29,15 @@ void input_sudoku(int *diff_level,int *no_sudoku){
     printf("....................Welcome to Sudoku Generator......................\n\n");     
     int x;
     int y;
+    float a;
+    float b;
     int i=5;
     int j=5;
     while(i!=0) {/*Asks the user for difficulty level.*/
         printf("Enter difficulty level as(0,1,2 or 3):\nEasy:      0\nMedium:    1\nHard:      2\nVery Hard: 3\n->");
-	scanf("%d",&x);
-        if(x<=3&&x>=0) {/* Checks whether the difficulty level is in the specified range*/
+	scanf("%f",&a);
+        x=a;
+        if(x<=3&&x>=0&&x==a) {/* Checks whether the difficulty level is in the specified range*/
             *diff_level=x;
             printf("..............................................................................\n");
             break;
@@ -50,8 +53,9 @@ void input_sudoku(int *diff_level,int *no_sudoku){
     }
     while(j!=0) {/*Asks the user for number of sudokus.*/
         printf("Enter the number of sudokus you would like to print between 1 to 40:\n->");
-        scanf("%d",&y);
-        if(y<=40&&y>=1) {/* Checks whether the number of sudokus are in the specified range*/
+        scanf("%f",&b);
+        y=b;
+        if(y<=40&&y>=1&&y==b) {/* Checks whether the number of sudokus are in the specified range*/
             *no_sudoku=y;
             break;
         }else {/*If number of sudokus are not in the specified range*/
