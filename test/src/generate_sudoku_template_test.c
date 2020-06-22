8a0daba7f895b92e3d3bd13aada53bf385a039fe
Mathieufalardeau3 <65470001+Mathieufalardeau3@users.noreply.github.com>
2020-06-21 22:32:34 -0400

    Revert "Revert "Revert "Merge branch 'development' of https://github.com/Prasanthkota165/Group_H_SUDOKU-GENERATOR into development"""
    This reverts commit b165aab3262f494cbf3a9b4c67a990b092979b01.

--- test/src/generate_sudoku_template_test.c
+++ test/src/generate_sudoku_template_test.c
@@ -1,53 +1,31 @@
+#include <stdio.h>
+#include "../../include/sudoku.h"
+//#include "../../src/generate_sudoku_template.c"
+
 /*
 * TITLE      : generate_sudoku_template_test.c 
 *
-* Purpose   : Tests the function generate_sudoku_template.c
+* Purpose   : Generates a pre-defined sudoku templete
 *
 * Language  : C
 *
 * Author    : Rishith Rao, 
 * GitHub ID : rishithrao57 
-* Date      : 21 June 2020
+* Date      : 7 June 2020
 *
 **/
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
-#include<time.h>
-#include "../../include/sudoku.h"
-
-int main() {
-    char* log_location = "../log/generate_sudoku_template.log"; /*Define the path*/
-    FILE *log = fopen(log_location, "w"); /*Write access to the log file */
-    srand(time(0));
-    int sudoku_temp[9][9]={0};
-    int count=0;
-    _Bool b;
-    generate_sudoku_template(sudoku_temp);
-    int i,j;
-    fprintf(log,".........The function will return this matrix.............\n\n");
-    for(i=0;i<9;i++){
-        fprintf(log,"   ");
+int main(void) {
+  int sudoku_temp[9][9];
+  generate_sudoku_template(sudoku_temp);
+  int i,j;
+for(i=0;i<9;i++){
 	for(j=0;j<9;j++){
-	    fprintf(log,"%d ",sudoku_temp[i][j]);
-        }
-        fprintf(log,"\n");
-    }
+		printf("%d ",sudoku_temp[i][j]);
+	}
+	printf("\n");
+}
 
-    for(i=0;i<9;i++){
-       	for(j=0;j<9;j++){
-	    if(sudoku_temp[i][j]==0){
-                count++;
-            }
-        }
-    }
-    if(count == 0 ){
-        fputs("\n\nThe function successfully generated and returned the 9X9 matrix",log);
-    } else { 
-        fputs("Function failed to generated and returned the 9X9 matrix",log); 
-    }
-    fclose(log);
-    return 0;
+return 0;
+
 }
